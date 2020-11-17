a064dae58aebaac77a23c771d0aafaf5d7b9fc9a
khermalik <khermalik@gmail.com>
2020-11-16 20:19:00 -0800

    adds custom markers to map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,31 +4,37 @@ import React from 'react';
 import { Loader } from "@googlemaps/js-api-loader";
 
 const Map = (props) => {
-    loader.load().then(() => {
-      let map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 34.0522, lng: -118.2437 },
-        zoom: 8,
+  loader.load().then(() => {
+    let map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 34.0522, lng: -118.2437 },
+      zoom: 8,
+    });
+    function addMarker(earthquake) { 
+      const icon = {
+        url: 'https://i.ibb.co/7tXznS3/earthquake.png',
+        scaledSize: new google.maps.Size(30, 30),
+      }; 
+      new google.maps.Marker({
+        position: { 
+          lat: earthquake.geometry.coordinates[1], 
+          lng: earthquake.geometry.coordinates[0] 
+        },
+        map: map,
+        icon: icon,
       });
-      function addMarker(earthquake) {  
-        new google.maps.Marker({
-            position: { lat: earthquake.geometry.coordinates[1], lng: earthquake.geometry.coordinates[0] },
-            map: map,
-          });
-      }
-      props.earthquakes.features !== undefined &&
-      (props.earthquakes.features).forEach(addMarker)
-      });
+    }
+    props.earthquakes.features &&
+    (props.earthquakes.features).forEach(addMarker)
+  });
   
-    return (
-      <div id="map"></div>
-    );
-  }
+  return (
+    <div id="map"></div>
+  );
+}
 
-  
 const loader = new Loader({
   apiKey: "AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg",
   version: "weekly",
 });
-
 
 export default Map;
--- src/Components/Quakes.js
+++ src/Components/Quakes.js
@@ -3,11 +3,12 @@ import React from 'react';
 function Quakes(props) {
   return(
     <div id="info">
-    { props.earthquakes.features !== undefined && 
+    {props.earthquakes.features && 
       (props.earthquakes.features).map((earthquake) => (
-        <p key={ earthquake.properties.ids }>{ earthquake.properties.title }</p>
-      ))
-    }
+        <p key={ earthquake.properties.ids }>
+          { earthquake.properties.title }
+        </p>
+      ))}
     </div>
   )
 };
